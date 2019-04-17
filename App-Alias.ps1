121af75a56b7dd8aa5ea4033c729f778521b53e6
Dmitry Stadub <dev@eml.by>
2019-04-18 00:47:00 +0300

    my app aliases

---
+++ App-Alias.ps1
@@ -0,0 +1,3 @@
+Set-Alias npp "C:\Program Files\Notepad++\notepad++.exe" -Scope Global
+Set-Alias np "C:\Program Files\Sublime Text 3\sublime_text.exe"  -Scope Global
+Set-Alias ssh "C:\Windows\System32\OpenSSH\ssh.exe" -Scope Global
