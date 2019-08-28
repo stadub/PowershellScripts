bb1eeba3b6e6814a6e209967723ac074d9b44d14
Dmitry Stadub <dev@eml.by>
2019-08-28 18:46:33 +0300

    remove extra shared functions & loader

--- Bookmarks/Bookmarks.psm1
+++ Bookmarks/Bookmarks.psm1
@@ -1,15 +1,15 @@
-if($ModuleDevelopment){
-    . $PSScriptRoot\..\Shared-Functions\ModuleLoader.ps1 "$PSScriptRoot"
-}
-else {
-    if( -not $SharedLocal -or -not (Test-Path $SharedLocal) ){
-        throw "Shared functions folder doesn't exist. 
-        Looks like module typing to start in Develoment mode. 
-        To start Development mode set flag: `$ModuleDevelopment=`$true
-        Also you can set `$DebugPreference=`"Continue`" to getmore detalied strack trace
-        Or set `$NoExport=`$True to disable `Export-ModuleMember` functions
-        "
-    }
-    . $PSScriptRoot\Shared\ModuleLoader.ps1 "$PSScriptRoot"
-}
+﻿. $PSScriptRoot\Functions.ps1
+. $PSScriptRoot\Shared-Functions.ps1
 
+
+_Initalize
+
+Export-ModuleMember -Function Add-PSBookmark
+Export-ModuleMember -Function Remove-PSBookmark
+Export-ModuleMember -Function Remove-AllPSBookmarks
+Export-ModuleMember -Function Open-PSBookmark
+Export-ModuleMember -Function Restore-PSBookmarks
+Export-ModuleMember -Function Save-PSBookmarks
+Export-ModuleMember -Function Get-PSBookmarks
+Export-ModuleMember -Function Update-PSBookmark 
+. $PSScriptRoot\Aliases.ps1
